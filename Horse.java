6a61327febd00afd923dd0170507ecb6ef1be675
testfiu <114039460+fiufreer@users.noreply.github.com>
2022-10-05 03:19:34 +0000

    Added the answers.

--- Horse.java
+++ Horse.java
@@ -1,1 +1,18 @@
 //Create the Horse class here.
+//Create the Horse class here.
+
+public class Horse{
+    public String nameOfHorse;
+    public double weightOfHorse;
+    public boolean isHorseTame;
+
+    public Horse(String nameOfHorse, double weightOfHorse, boolean isHorseTame) {
+        this.nameOfHorse = nameOfHorse;
+        this.weightOfHorse = weightOfHorse;
+        this.isHorseTame = isHorseTame;
+    }
+
+    public String toString(){
+        return "Name: " + this.nameOfHorse + " Weight: " + this.weightOfHorse + " Tame: " + this.isHorseTame;
+    }
+}
--- Stable.java
+++ Stable.java
@@ -1,1 +1,12 @@
 //Create the Stable class here.
+//Create the Stable class here.
+
+import java.util.ArrayList;
+
+public class Stable{
+    public String address;
+    public ArrayList<Horse> arrayList = new ArrayList<>();
+
+
+
+}
